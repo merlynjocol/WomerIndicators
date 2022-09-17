0356aac6f241e97f1372c0c29947764d6466bb6c
merlynjocol <45790494+merlynjocol@users.noreply.github.com>
2022-09-17 17:27:35 +0200

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,49 @@
+"""
+Streamlit App
+"""
+# pylint: disable=wrong-import-order
+
+import streamlit as st
+from apps import information, natural_resources, climate, about, participation, knowledge 
+from streamlit_option_menu import option_menu
+from PIL import Image
+
+from typing import Callable
+
+
+st.set_page_config(page_title="Womer", page_icon="🌍", layout="wide")
+
+
+apps = [
+    {"func": information.app, "title": "Datos básicos", "icon": "house"},
+    {"func": about.app, "title": "Acerca de", "icon": "info"},
+]
+
+titles = [app["title"] for app in apps]
+icons = [app["icon"] for app in apps]
+
+params = st.experimental_get_query_params()
+
+if "page" in params:
+    DEFAULT_INDEX = titles.index(params["page"][0].lower())
+else:
+    DEFAULT_INDEX = 0
+
+with st.sidebar:
+    logo = Image.open("images/WOMER_LOGO HORIZONTAL.png")
+    st.image(logo, use_column_width=True)
+
+    selected = option_menu(
+        "TEMA",
+        options=titles,
+        icons=icons,
+        menu_icon="list",
+        default_index=DEFAULT_INDEX,
+    )
+
+
+for app in apps:
+    if app["title"] == selected:
+        page_func: Callable = app["func"]
+        page_func()
+        break
